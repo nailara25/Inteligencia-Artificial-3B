833c30cb483f9691da8c82bdb5305f9f1fa79cea
naíma <104856987+nailara25@users.noreply.github.com>
2024-07-26 10:41:27 -0300

    x

---
+++ main.js
@@ -0,0 +1,19 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "Você gosta de bolo?",
+        alternativas: [
+            {
+                texto: "Alternativa A",
+                afirmacao: "Você é uma pessoa normal"
+            },
+            {
+                texto: "Alternativa B",
+                afirmacao: "Você é chato pra carambolas"
+            }]
+        
+    },
+    pergunta2{
+
+
+    }
+]
